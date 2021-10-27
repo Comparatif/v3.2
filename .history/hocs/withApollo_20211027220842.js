63554f7ffea952138fd45f82f66d467b92bcf967
Allocerveau <75397817+Allocerveau@users.noreply.github.com>
2021-10-28 00:36:17 +0100

    SEOv3

---
+++ .history/hocs/withApollo_20211027220842.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import withApollo from 'next-with-apollo'
+import { InMemoryCache, ApolloProvider, ApolloClient, createHttpLink } from '@apollo/client'
+import { useRouter } from 'next/router'
+
+
+const isBrowser = typeof window !== `undefined`
+const host = isBrowser ? (window.location.host == 'localhost:3000' ? 'http://localhost:3000': 'https://comparatifdz.com') : ''
+
+export default withApollo(
+  
+  ({ initialState, headers }) => {
+    
+    var str = 'https://comparatifdz.com/3200G'
+    //var new_str = str.substring(0, str.indexOf("/")+10);
+    var new_str1 = str.split("/");
+    var new_str2 = new_str1[0] + '//' + new_str1[2] + ;
+    console.log(new_str)
+
+    const cache = new InMemoryCache({}).restore(initialState || {})
+    
+    if (isBrowser) window.cache = cache
+
+    
+    
+    return new ApolloClient({
+      ssrMode: true,
+      
+      link: createHttpLink({
+        
+        uri: 'http://localhost:3000/api/graphql',
+       // uri: 'http://localhost:3000/api/graphql',
+        credentials: 'same-origin',
+        headers: {
+          cookie: headers?.cookie
+        }
+      }),
+      cache
+    })
+  },
+  {
+    render: ({ Page, props }) => (
+      <ApolloProvider client={props.apollo}>
+        <Page {...props} />
+      </ApolloProvider>
+    )
+  }
+)
