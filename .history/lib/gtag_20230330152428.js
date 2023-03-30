f078b3fce3767accde385220e405d196aed74e7c
Allocerveau <samiboudehri@hotmail.fr>
2023-03-30 15:26:55 +0100

    v3.1

--- .history/lib/gtag_20230330152428.js
+++ .history/lib/gtag_20230330152428.js
@@ -1,7 +1,7 @@
 const isBrowser = typeof window !== `undefined`
 
 //export const GA_TRACKING_ID = isBrowser ? (window.location.host == 'localhost:3000' ? '': 'G-5YNNC96PGE')  : ''
-export const GA_TRACKING_ID = typeof document !== 'undefined' ?  document.location.hostname == "comparatifdz.com" ? 'G-5YNNC96PGE' : '' : ''
+export const GA_TRACKING_ID = if (typeof document !== 'undefined').hostname == "comparatifdz.com" ? 'G-5YNNC96PGE' : ''
 
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
